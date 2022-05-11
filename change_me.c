3b2b51c8b21f46429e62467146eae033dbd7669e
CptBozkov <martindouda4444@seznam.cz>
2022-05-11 13:27:30 +0200

    functions

--- change_me.c
+++ change_me.c
@@ -9,6 +9,7 @@
 #include <math.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include </usr/include/libusb-1.0/libusb.h>
 
 #include "mzapo_parlcd.h"
 #include "mzapo_phys.h"
@@ -16,6 +17,8 @@
 
 #define SCREEN_SIZE_X 480
 #define SCREEN_SIZE_Y 320
+
+#define BASE_PLAYER_SPEED 2
 
 #define FPS 60
 
@@ -35,18 +38,37 @@ typedef union pixel {
     uint16_t d;
 } pixel;
 
+typedef union knobs {
+    struct {
+        uint8_t b, g, r;
+        unsigned b_p : 1;
+        unsigned g_p : 1;
+        unsigned r_p : 1;
+    };
+    uint32_t d;
+} knobs;
+
+typedef struct player {
+    float x;
+    float y;
+    float speed;
+    int d_x;
+    int d_y;
+    pixel color;
+} player;
+
 // tenhle struct bude slouzit k prenosu dat mezi thready
 // pri volani funkce na spusteni thread mu muzes predat jeden void pointer (v tomhle pripade jsem tam dal struct data_passer)
 typedef struct data_passer{
-    pixel *buffer1;
-    pixel *buffer2;
+    pixel *game_buffer;
+    pixel *menu_buffer;
     int drawnBuffer;
 
     bool draw;
     bool doneDraw;
 
     bool run;
-} data_passer_t;
+} data_passer;
 
 
 void addToBuffer(unsigned x, unsigned y, pixel *p, pixel *buffer){
@@ -78,77 +100,109 @@ void clearBuffer(pixel *buffer) {
     }
 }
 
+pixel * createPixel(int r, int b, int g){
+    pixel * p = malloc(sizeof(pixel));
+    p->r = r;
+    p->g = g;
+    p->b = b;
+    return p;
+}
 
-void gameLoop(data_passer_t * dp, struct timespec *start, struct timespec *end, struct timespec *res){
-    // az tady nadefinujem vsechny promenny
+player * createPlayer(int x, int y, pixel * color){
+    player * p = malloc(sizeof(player));
+    p->x = x;
+    p->y = y;
+    p->d_x = 1;
+    p->d_y = 1;
+    p->speed = BASE_PLAYER_SPEED;
+    p->color = *color;
+    return p;
+}
+
+data_passer * createDataPasser(){
+    data_passer * dp = malloc(sizeof(data_passer));
+    dp->game_buffer = malloc(SCREEN_SIZE_X*SCREEN_SIZE_Y*sizeof(pixel));
+    dp->menu_buffer = malloc(SCREEN_SIZE_X*SCREEN_SIZE_Y*sizeof(pixel));
+    dp->drawnBuffer = 1;
+    dp->draw = false;
+    dp->doneDraw = true;
+    dp->run = true;
+    return dp;
+}
 
 
-    volatile void *spiled_reg_base = map_phys_address(SPILED_REG_BASE_PHYS, SPILED_REG_SIZE, 0);
-    volatile uint32_t *ledline = (spiled_reg_base + SPILED_REG_LED_LINE_o);
-    volatile uint32_t *rgb1 = (spiled_reg_base + SPILED_REG_LED_RGB1_o);
-    volatile uint32_t *knobs = (spiled_reg_base + SPILED_REG_KNOBS_8BIT_o);
+void updatePlayer(player * p){
+    if (p->x > SCREEN_SIZE_X || p->x < 0){
+        p->d_x *= -1;
+    }
+    if (p->y > SCREEN_SIZE_Y || p->y < 0){
+        p->d_y *= -1;
+    }
+    p->x += p->d_x * p->speed;
+    p->y += p->d_y * p->speed;
+}
 
-    *ledline = 0x80000001;
-    *rgb1 = ((union led){.r = 0x10, .g = 0x10, .b = 0x10}).d;
+void drawPlayer(player * p, pixel * buffer){
+    drawCircle((int) p->x, (int) p->y, 10, &p->color, buffer);
+}
 
-
+void gameLoop(data_passer * dp, struct timespec *start, struct timespec *end, struct timespec *res){
     void *parlcd_reg_base = map_phys_address(PARLCD_REG_BASE_PHYS, PARLCD_REG_SIZE, 0);
+    volatile void *spiled_reg_base = map_phys_address(SPILED_REG_BASE_PHYS, SPILED_REG_SIZE, 0);
     parlcd_write_cmd(parlcd_reg_base, 0x2c);
-
-    pixel p;
-    p.r = 0xff;
-    p.g = 0xff;
-    p.b = 0x00;
-    pixel p2;
-    p.r = 0x00;
-    p.g = 0xff;
-    p.b = 0x00;
+    volatile uint32_t *ledline = (spiled_reg_base + SPILED_REG_LED_LINE_o);
+    *ledline = 0x80000001;
+    volatile uint32_t *rgb1 = (spiled_reg_base + SPILED_REG_LED_RGB1_o);
+    *rgb1 = ((union led){.r = 0x10, .g = 0x10, .b = 0x10}).d;
+    volatile uint32_t *knobs_input = (spiled_reg_base + SPILED_REG_KNOBS_8BIT_o);
+    knobs k;
 
 
-    float x = SCREEN_SIZE_X/2;
-    float y = SCREEN_SIZE_Y/2;
-    float d_x = 5;
-    float d_y = 5;
 
-    pixel * b = dp->buffer1;
+    pixel * b;
 
-    clearBuffer(dp->buffer1);
-    clearBuffer(dp->buffer2);
+    pixel * pixel = createPixel(0xff, 0xff, 0x00);
 
+    player * player1 = createPlayer(SCREEN_SIZE_X/2, SCREEN_SIZE_Y/2, createPixel(0xff, 0xff, 0x00));
+    player * player2 = createPlayer(0, SCREEN_SIZE_Y/2, createPixel(0xff, 0x00, 0x00));
+
+
+    clearBuffer(dp->game_buffer);
+    clearBuffer(dp->menu_buffer);
 
     while (dp->run){
         if (dp->doneDraw) {
             clock_gettime(CLOCK_MONOTONIC, start);
 
             // sem prijde celej nas main loop
-            //ty funkce nad a po printem drzej konstantni FPS
             dp->doneDraw = false;
             dp->draw = true;
 
-            b = dp->buffer1;
+            b = dp->game_buffer;
             /*dp->drawnBuffer *= -1;
             if (dp->drawnBuffer == 1) {
-                b = dp->buffer1;
+                b = dp->game_buffer;
             } else {
-                b = dp->buffer2;
+                b = dp->menu_buffer;
             }*/
 
-            //clearBuffer(b);
-            //drawCircle((int) x, (int) y, 10, &p2, b);
-            // do stuff depending on key_code
+            k.d = *knobs_input;
+            pixel->r = k.r;
+            pixel->g = k.g;
+            pixel->b = k.b;
 
-            drawCircle((int) x, (int) y, 10, &p, b);
-
-
-            if (x > SCREEN_SIZE_X || x < 0){
-                d_x *= -1;
+            /*if (k.r_p){
+                speed += 1;
             }
-            if (y > SCREEN_SIZE_Y || y < 0){
-                d_y *= -1;
-            }
+            if (k.b_p){
+                speed -= 1;
+            }*/
 
-            x += d_x;
-            y += d_y;
+            updatePlayer(player1);
+            updatePlayer(player2);
+            drawPlayer(player1, b);
+            drawPlayer(player2, b);
+
 
             // tady konci nas main loop
 
@@ -164,7 +218,7 @@ void gameLoop(data_passer_t * dp, struct timespec *start, struct timespec *end, 
     dp->run = false;
 }
 
-void draw(data_passer_t * dp){
+void draw(data_passer * dp){
     void *parlcd_reg_base = map_phys_address(PARLCD_REG_BASE_PHYS, PARLCD_REG_SIZE, 0);
 
     pixel * b;
@@ -173,11 +227,11 @@ void draw(data_passer_t * dp){
         if (dp->draw){
             dp->draw = false;
 
-            b = dp->buffer1;
+            b = dp->game_buffer;
             /*if (dp->drawnBuffer == 1) {
-                b = dp->buffer2;
+                b = dp->menu_buffer;
             } else {
-                b = dp->buffer1;
+                b = dp->game_buffer;
             }*/
 
             for (unsigned i = 0; i <480*320; i++){
@@ -190,7 +244,7 @@ void draw(data_passer_t * dp){
 
 // funkce kterou predavam gameLoopThread
 void* gameLoopThread(void * dp_void){
-    data_passer_t * dp = (data_passer_t *) dp_void;
+    data_passer * dp = (data_passer *) dp_void;
     struct timespec start = {0,0};
     struct timespec end = {0,0};
     struct timespec res;
@@ -200,27 +254,17 @@ void* gameLoopThread(void * dp_void){
 
 // funkce kterou predavam drawThread
 void* drawThread(void * dp_void){
-    data_passer_t * dp = (data_passer_t *) dp_void;
+    data_passer * dp = (data_passer *) dp_void;
     draw(dp);
     return NULL;
 }
 
 int main (){
-    data_passer_t * dp = malloc(sizeof(data_passer_t));
-    dp->buffer1 = malloc(SCREEN_SIZE_X*SCREEN_SIZE_Y*sizeof(pixel));
-    dp->buffer2 = malloc(SCREEN_SIZE_X*SCREEN_SIZE_Y*sizeof(pixel));
-
-    dp->drawnBuffer = 1;
-
-    dp->draw = false;
-    dp->doneDraw = true;
-
-    dp->run = true;
+    data_passer * dp = createDataPasser();
 
     pthread_t tid0;
     pthread_t tid1;
 
-    // musime pointery pretypovat na void a pak zas zpatky na data_passer_t
     pthread_create(&tid0, NULL, gameLoopThread, (void *)dp);
     pthread_create(&tid1, NULL, drawThread, (void *)dp);
 
